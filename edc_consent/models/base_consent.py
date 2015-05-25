0839850a1e30a07780547b3e19479b14ffcc64f6
erik van widenfelt <ew2789@gmail.com>
2015-05-25 10:57:23 +0200

    setup

--- edc_consent/models/base_consent.py
+++ edc_consent/models/base_consent.py
@@ -2,24 +2,24 @@ import re
 
 from dateutil.relativedelta import relativedelta
 
-from django.db import models
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-from edc.core.crypto_fields.fields import EncryptedLastnameField, EncryptedTextField
-from edc.core.crypto_fields.utils import mask_encrypted
-from edc.core.bhp_variables.models import StudySite
+from django_crypto_fields.fields import LastnameField, EncryptedTextField
+from django_crypto_crypto_fields.utils import mask_encrypted
+
 from edc.core.bhp_common.utils import formatted_age
-from edc.base.model.validators import datetime_not_future, datetime_not_before_study_start, eligible_if_no
-from edc.choices.common import YES_NO
 from edc.subject.subject.models import BaseSubject
+from edc_base.model.validators import datetime_not_future, datetime_not_before_study_start, eligible_if_no
+from edc_constants.choices import YES_NO
 
-from edc_consent import ConsentError
-from edc_consent import ConsentedSubjectIdentifier
+from ..classes import ConsentedSubjectIdentifier
+from ..exceptions import ConsentError
 
-from edc_consent import BaseConsentHistory
+from .base_consent_history import BaseConsentHistory
 
 
 # allow a settings attribute to override the unique constraint on the
@@ -40,37 +40,39 @@ class BaseConsent(BaseSubject):
         blank=True,
         db_index=True,
         unique=subject_identifier_is_unique,
-        )
+    )
 
-    study_site = models.ForeignKey(StudySite,
+    site_code = models.CharField(
         verbose_name='Site',
-        null=True,
         help_text="This refers to the site or 'clinic area' where the subject is being consented."
-        )
+    )
 
     consent_datetime = models.DateTimeField(
         verbose_name="Consent date and time",
         validators=[
             datetime_not_before_study_start,
             datetime_not_future, ],
-        )
+    )
 
-    guardian_name = EncryptedLastnameField(
+    guardian_name = LastnameField(
         verbose_name=("Guardian\'s Last and first name (minors only)"),
         validators=[
-            RegexValidator('^[A-Z]{1,50}\, [A-Z]{1,50}$', 'Invalid format. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma'),
-            ],
+            RegexValidator(
+                '^[A-Z]{1,50}\, [A-Z]{1,50}$',
+                'Invalid format. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma')],
         blank=True,
         null=True,
-        help_text=('Required only if subject is a minor. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma then followe by a space.'),
-        )
+        help_text=(
+            'Required only if subject is a minor. Format is \'LASTNAME, FIRSTNAME\'. '
+            'All uppercase separated by a comma then followe by a space.'),
+    )
 
     may_store_samples = models.CharField(
         verbose_name=_("Sample storage"),
         max_length=3,
         choices=YES_NO,
         help_text=("Does the subject agree to have samples stored after the study has ended")
-        )
+    )
 
     is_incarcerated = models.CharField(
         verbose_name="Is the participant under involuntary incarceration?",
@@ -79,7 +81,7 @@ class BaseConsent(BaseSubject):
         validators=[eligible_if_no, ],
         default='-',
         help_text="( if 'YES' STOP patient cannot be consented )",
-        )
+    )
 
     is_literate = models.CharField(
         verbose_name="Is the participant LITERATE?",
@@ -87,35 +89,39 @@ class BaseConsent(BaseSubject):
         choices=YES_NO,
         default='-',
         help_text="( if 'No' provide witness\'s name here and with signature on the paper document.)",
-        )
+    )
 
     witness_name = EncryptedLastnameField(
         verbose_name=_("Witness\'s Last and first name (illiterates only)"),
         validators=[
-            RegexValidator('^[A-Z]{1,50}\, [A-Z]{1,50}$', 'Invalid format. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma'),
-            ],
+            RegexValidator(
+                '^[A-Z]{1,50}\, [A-Z]{1,50}$',
+                'Invalid format. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma')],
         blank=True,
         null=True,
-        help_text=_('Required only if subject is illiterate. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated by a comma'),
-        )
+        help_text=_(
+            'Required only if subject is illiterate. Format is \'LASTNAME, FIRSTNAME\'. '
+            'All uppercase separated by a comma'),
+    )
 
-    comment = EncryptedTextField("Comment",
+    comment = EncryptedTextField(
+        verbose_name="Comment",
         max_length=250,
         blank=True,
         null=True
-        )
+    )
 
     consent_version_on_entry = models.IntegerField(
         editable=False,
         default=1,
         help_text='Version of subject\'s initial edc_consent.'
-        )
+    )
 
     consent_version_recent = models.IntegerField(
         editable=False,
         default=1,
         help_text='Version of subject\'s most recent edc_consent.'
-        )
+    )
 
     language = models.CharField(
         verbose_name='Language of edc_consent',
@@ -123,22 +129,17 @@ class BaseConsent(BaseSubject):
         choices=settings.LANGUAGES,
         default='not specified',
         help_text='The language used for the edc_consent process will also be used during data collection.'
-        )
+    )
 
     is_verified = models.BooleanField(default=False, editable=False)
 
     is_verified_datetime = models.DateTimeField(null=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return "{0} {1} {2}".format(self.mask_unset_subject_identifier(), mask_encrypted(self.first_name), self.initials)
 
     def get_site_code(self):
-        site_code = ''
-        try:
-            site_code = self.study_site.site_code
-        except AttributeError:
-            raise ImproperlyConfigured('Site code may not be None. Either include the field for user input or override method get_site_code() on the concrete model.')
-        return site_code
+        return self.site_code
 
     def save_new_consent(self, using=None, subject_identifier=None):
         """ Users may override this to compliment the default behavior for new instances.
@@ -209,7 +210,7 @@ class BaseConsent(BaseSubject):
             'first_name': self.first_name,
             'last_name': self.last_name,
             'subject_type': self.get_subject_type(),
-            }
+        }
         if self.last_name:
             options.update({'registration_status': 'consented'})
         return options
@@ -223,20 +224,27 @@ class BaseConsent(BaseSubject):
 
     @classmethod
     def get_consent_update_model(self):
-        raise TypeError('The ConsentUpdateModel is required. Specify a class method get_consent_update_model() on the model to return the ConsentUpdateModel class.')
+        raise TypeError(
+            'The ConsentUpdateModel is required. Specify a class method '
+            'get_consent_update_model() on the model to return the ConsentUpdateModel class.')
 
     def get_report_datetime(self):
         return self.consent_datetime
 
     def get_subject_type(self):
-        raise ImproperlyConfigured('Method must be overridden to return a subject_type. e.g. \'subject\', \'maternal\', \'infant\', etc')
+        raise ImproperlyConfigured(
+            'Method must be overridden to return a subject_type. '
+            'e.g. \'subject\', \'maternal\', \'infant\', etc')
 
     def bypass_for_edit_dispatched_as_item(self, using=None, update_fields=None):
         """Allow bypass only if doing edc_consent verification."""
         # requery myself
         obj = self.__class__.objects.using(using).get(pk=self.pk)
         # dont allow values in these fields to change if dispatched
-        may_not_change_these_fields = [(k, v) for k, v in obj.__dict__.iteritems() if k not in ['is_verified_datetime', 'is_verified']]
+        may_not_change_these_fields = []
+        for k, v in obj.__dict__.items():
+            if k not in ['is_verified_datetime', 'is_verified']:
+                may_not_change_these_fields.append((k, v))
         for k, v in may_not_change_these_fields:
             if k[0] != '_':
                 if getattr(self, k) != v:
