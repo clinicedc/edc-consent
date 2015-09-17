2bfc0469fe41d40c676183ec92a79a6b16807dc0
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-17 14:44:22 +0200

    removing attempt to translate questions which breaks the text in form

--- edc_consent/models/fields/sample_collection_fields_mixin.py
+++ edc_consent/models/fields/sample_collection_fields_mixin.py
@@ -6,7 +6,7 @@ from edc_constants.choices import YES_NO
 class SampleCollectionFieldsMixin(models.Model):
 
     may_store_samples = models.CharField(
-        verbose_name=_("Sample storage"),
+        verbose_name="Sample storage",
         max_length=3,
         choices=YES_NO,
         help_text=("Does the subject agree to have samples stored after the study has ended")
