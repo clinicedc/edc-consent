a5916d0a123918413be3a23961f4535caa3394ac
Florence Chilisa <fchilisa@gmail.com>
2015-11-20 14:27:41 +0200

    add consent copy

--- edc_consent/models/fields/sample_collection_fields_mixin.py
+++ edc_consent/models/fields/sample_collection_fields_mixin.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from edc_constants.choices import YES_NO
+from edc_constants.choices import YES_NO, YES_NO_DECLINED
+from edc_consent.models.validators import eligible_if_yes_or_declined
 
 
 class SampleCollectionFieldsMixin(models.Model):
@@ -10,6 +11,16 @@ class SampleCollectionFieldsMixin(models.Model):
         max_length=3,
         choices=YES_NO,
         help_text=("Does the subject agree to have samples stored after the study has ended")
+    )
+
+    consent_copy = models.CharField(
+        verbose_name=("I have provided the client with a copy of their signed informed consent"),
+        max_length=20,
+        choices=YES_NO_DECLINED,
+        validators=[eligible_if_yes_or_declined, ],
+        null=True,
+        blank=False,
+        help_text="If declined, return copy to the clinic with the consent",
     )
 
     class Meta:
