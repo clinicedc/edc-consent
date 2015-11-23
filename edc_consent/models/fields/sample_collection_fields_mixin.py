b2d0af95c7e2083982b2a72056c030fcb10668f0
Florence Chilisa <fchilisa@gmail.com>
2015-11-23 15:00:19 +0200

    remove consent copy

--- edc_consent/models/fields/sample_collection_fields_mixin.py
+++ edc_consent/models/fields/sample_collection_fields_mixin.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-from edc_constants.choices import YES_NO, YES_NO_DECLINED
-from edc_consent.models.validators import eligible_if_yes_or_declined
+from edc_constants.choices import YES_NO
 
 
 class SampleCollectionFieldsMixin(models.Model):
@@ -11,16 +10,6 @@ class SampleCollectionFieldsMixin(models.Model):
         max_length=3,
         choices=YES_NO,
         help_text=("Does the subject agree to have samples stored after the study has ended")
-    )
-
-    consent_copy = models.CharField(
-        verbose_name=("I have provided the client with a copy of their signed informed consent"),
-        max_length=20,
-        choices=YES_NO_DECLINED,
-        validators=[eligible_if_yes_or_declined, ],
-        null=True,
-        blank=False,
-        help_text="If declined, return copy to the clinic with the consent",
     )
 
     class Meta:
