68968f3ef8c80cd8acb2cb0be18f460e387246c8
erikvw <ew2789@gmail.com>
2016-06-06 23:12:08 +0200

    fix app config for migrate

--- edc_consent/apps.py
+++ edc_consent/apps.py
@@ -3,8 +3,34 @@ import sys
 from django.apps import AppConfig
 from django.conf import settings
 from django.apps import apps as django_apps
+from django.db.models.signals import post_migrate
 
 from edc_configuration.convert import localize
+from django.db.utils import OperationalError
+
+
+def update_or_create_consent_type(sender, **kwargs):
+    sys.stdout.write('Loading {} ...\r'.format(sender.verbose_name))
+    done_message = 'Loading {} ... Done.\n'.format(sender.verbose_name)
+    ConsentType = django_apps.get_model(sender.name, 'consenttype')
+    for item in sender.consent_type_setup:
+        if settings.USE_TZ:
+            item['start_datetime'] = localize(item.get('start_datetime'))
+            item['end_datetime'] = localize(item.get('end_datetime'))
+        try:
+            consent_type = ConsentType.objects.get(
+                version=item.get('version'),
+                app_label=item.get('app_label'),
+                model_name=item.get('model_name'))
+            consent_type.start_datetime = item.get('start_datetime')
+            consent_type.end_datetime = item.get('end_datetime')
+            consent_type.save()
+        except ConsentType.DoesNotExist:
+            ConsentType.objects.create(**item)
+            done_message = '\n'
+            sys.stdout.write('\n * added {}.{} version {}'.format(
+                item.get('app_label'), item.get('model_name'), item.get('version')))
+    sys.stdout.write(done_message)
 
 
 class EdcConsentAppConfig(AppConfig):
@@ -13,27 +39,7 @@ class EdcConsentAppConfig(AppConfig):
     consent_type_setup = None
 
     def ready(self):
-        sys.stdout.write('Loading {} ...\r'.format(self.verbose_name))
-        self.update_or_create_consent_type()
-
-    def update_or_create_consent_type(self):
-        done_message = 'Loading {} ... Done.\n'.format(self.verbose_name)
-        ConsentType = django_apps.get_model(self.name, 'consenttype')
-        for item in self.consent_type_setup:
-            if settings.USE_TZ:
-                item['start_datetime'] = localize(item.get('start_datetime'))
-                item['end_datetime'] = localize(item.get('end_datetime'))
-            try:
-                consent_type = ConsentType.objects.get(
-                    version=item.get('version'),
-                    app_label=item.get('app_label'),
-                    model_name=item.get('model_name'))
-                consent_type.start_datetime = item.get('start_datetime')
-                consent_type.end_datetime = item.get('end_datetime')
-                consent_type.save()
-            except ConsentType.DoesNotExist:
-                ConsentType.objects.create(**item)
-                done_message = '\n'
-                sys.stdout.write('\n * added {}.{} version {}'.format(
-                    item.get('app_label'), item.get('model_name'), item.get('version')))
-        sys.stdout.write(done_message)
+        try:
+            update_or_create_consent_type(self)
+        except OperationalError:
+            post_migrate.connect(update_or_create_consent_type, sender=self)
