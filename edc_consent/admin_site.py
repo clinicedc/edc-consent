cee1534e25c6207c06dccff2fd028a81f42c040e
erikvw <ew2789@gmail.com>
2016-11-01 15:54:02 -0500

    fix outdated import, move admin site to module

---
+++ edc_consent/admin_site.py
@@ -0,0 +1,9 @@
+from django.contrib.admin.sites import AdminSite
+
+
+class EdcConsentAdminSite(AdminSite):
+    site_header = 'Consent'
+    site_title = 'Consent'
+    index_title = 'Consent'
+    site_url = '/edc-consent/'
+edc_consent_admin = EdcConsentAdminSite(name='edc_consent_admin')
