834e729a8388be41234bbad07fd82124cabb988e
erikvw <ew2789@gmail.com>
2017-11-15 23:48:20 +0200

    remove edc_protocol imports

--- edc_consent/apps.py
+++ edc_consent/apps.py
@@ -24,8 +24,6 @@ class AppConfig(DjangoAppConfig):
 
 if settings.APP_NAME == 'edc_consent':
 
-    from edc_protocol.subject_type import SubjectType
-    from edc_protocol.cap import Cap
     from datetime import datetime
     from dateutil.tz.tz import gettz
     from edc_protocol.apps import AppConfig as BaseEdcProtocolAppConfig
@@ -35,10 +33,6 @@ if settings.APP_NAME == 'edc_consent':
         protocol_number = '099'
         protocol_name = 'TestApp'
         protocol_title = ''
-        subject_types = [
-            SubjectType('subject', 'Research Subject',
-                        Cap(model_name='edc_consent.subjectconsent', max_subjects=9999)),
-        ]
         study_open_datetime = datetime(
             2007, 12, 31, 0, 0, 0, tzinfo=gettz('UTC'))
         study_close_datetime = datetime(
