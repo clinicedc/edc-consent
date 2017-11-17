c6ccbfe4c6d7d607189987a7d917221d975f4c9d
erikvw <ew2789@gmail.com>
2017-11-17 08:07:46 +0200

    refactor common clean method

--- edc_consent/model_mixins.py
+++ edc_consent/model_mixins.py
@@ -31,6 +31,8 @@ class RequiresConsentMixin(models.Model):
 
     Requires attrs subject_identfier, report_datetime"""
 
+    require_consent_on_change = True
+
     consent_version = models.CharField(
         max_length=10, default='?', editable=False)
 
@@ -50,34 +52,35 @@ class RequiresConsentMixin(models.Model):
         return consent_object
 
     def common_clean(self):
-        consent_object = self.get_consent_object()
-        self.consent_version = consent_object.version
-        try:
-            subject_identifier = self.appointment.subject_identifier
-        except AttributeError:
-            subject_identifier = self.subject_identifier
-        try:
-            if not subject_identifier:
-                raise SiteConsentError(
-                    'Cannot lookup {} instance for subject. '
-                    'Got \'subject_identifier\' is None.'.format(
-                        consent_object.model._meta.label_lower))
-            options = dict(
-                subject_identifier=subject_identifier,
-                version=consent_object.version)
-            consent_object.model.objects.get(**options)
-        except consent_object.model.DoesNotExist:
-            raise NotConsentedError(
-                'Consent is required. Cannot find \'{consent_model} '
-                'version {version}\' when saving model \'{model}\' for '
-                'subject \'{subject_identifier}\' with date '
-                '\'{report_datetime}\' .'.format(
+        if not self.id or (self.id and self.require_consent_on_change):
+            consent_object = self.get_consent_object()
+            self.consent_version = consent_object.version
+            try:
+                subject_identifier = self.appointment.subject_identifier
+            except AttributeError:
+                subject_identifier = self.subject_identifier
+            try:
+                if not subject_identifier:
+                    raise SiteConsentError(
+                        'Cannot lookup {} instance for subject. '
+                        'Got \'subject_identifier\' is None.'.format(
+                            consent_object.model._meta.label_lower))
+                options = dict(
                     subject_identifier=subject_identifier,
-                    consent_model=consent_object.model._meta.label_lower,
-                    model=self._meta.label_lower,
-                    version=consent_object.version,
-                    report_datetime=self.report_datetime.strftime(
-                        '%Y-%m-%d %H:%M%z')))
+                    version=consent_object.version)
+                consent_object.model.objects.get(**options)
+            except consent_object.model.DoesNotExist:
+                raise NotConsentedError(
+                    'Consent is required. Cannot find \'{consent_model} '
+                    'version {version}\' when saving model \'{model}\' for '
+                    'subject \'{subject_identifier}\' with date '
+                    '\'{report_datetime}\' .'.format(
+                        subject_identifier=subject_identifier,
+                        consent_model=consent_object.model._meta.label_lower,
+                        model=self._meta.label_lower,
+                        version=consent_object.version,
+                        report_datetime=self.report_datetime.strftime(
+                            '%Y-%m-%d %H:%M%z')))
         super().common_clean()
 
     class Meta:
