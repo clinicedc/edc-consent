b48847c674af20e55995b1ec7b261036eca10233
erikvw <ew2789@gmail.com>
2024-01-31 20:15:08 -0600

    catch NotConsentedError in form, cdef from SubjectScreening

--- edc_consent/modelform_mixins/consent_modelform_mixin/consent_modelform_validation_mixin.py
+++ edc_consent/modelform_mixins/consent_modelform_mixin/consent_modelform_validation_mixin.py
@@ -7,11 +7,9 @@ from dateutil.relativedelta import relativedelta
 from django import forms
 from edc_constants.constants import NO, YES
 from edc_model_form.utils import get_field_or_raise
-from edc_sites.site import sites as site_sites
 from edc_utils import AgeValueError, age, formatted_age
 
-from ...exceptions import SiteConsentError
-from ...site_consents import ConsentDefinitionDoesNotExist, site_consents
+from ...exceptions import ConsentDefinitionValidityPeriodError
 from ...utils import InvalidInitials, verify_initials_against_full_name
 
 if TYPE_CHECKING:
@@ -29,23 +27,17 @@ class ConsentModelFormValidationMixin:
 
     @property
     def consent_definition(self) -> ConsentDefinition:
-        """Returns a ConsentDefinition instance or raises on
-        missing data.
+        """Returns a ConsentDefinition instance or raises
+        if consent date not within consent definition validity
+        period.
         """
-        current_site_id = getattr(
-            self.instance.site, "id", site_sites.get_current_site().site_id
-        )
-        try:
-            consent_definition = site_consents.get_consent_definition(
-                model=self._meta.model._meta.label_lower,
-                report_datetime=self.consent_datetime,
-                site=site_sites.get(current_site_id),
-            )
-        except (ConsentDefinitionDoesNotExist, SiteConsentError) as e:
-            raise forms.ValidationError(e)
-        if not consent_definition.version:
-            raise forms.ValidationError("Unable to determine consent version")
-        return consent_definition
+        cdef: ConsentDefinition = self.subject_screening.consent_definition
+        if self.consent_datetime:
+            try:
+                cdef.valid_for_datetime_or_raise(self.consent_datetime)
+            except ConsentDefinitionValidityPeriodError as e:
+                raise forms.ValidationError({"consent_datetime": str(e)})
+        return cdef
 
     def get_field_or_raise(self, name: str, msg: str) -> Any:
         return get_field_or_raise(
